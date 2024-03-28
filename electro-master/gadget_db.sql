07cb83a81309ab5e4f056a4f53f1ff39ded4ce62
Tan <tiangjw02@gmail.com>
2024-03-28 11:07:58 +0800

    add to db

--- electro-master/gadget_db.sql
+++ electro-master/gadget_db.sql
@@ -167,3 +167,64 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Table structure for table `paymentmethod`
+--
+
+CREATE TABLE `paymentmethod` (
+  `PaymentMID` int(40) NOT NULL,
+  `AccountNum` int(11) NOT NULL,
+  `ExpirationDate` date NOT NULL,
+  `SecurityCode` int(11) NOT NULL,
+  `BankName` varchar(255) NOT NULL,
+  `Pstatus` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paymentmethod`
+--
+
+INSERT INTO `paymentmethod` (`PaymentMID`, `AccountNum`, `ExpirationDate`, `SecurityCode`, `BankName`, `Pstatus`) VALUES
+(7, 1477, '2024-03-11', 12, '12', 'success'),
+(8, 12, '2024-03-11', 12, '12', 'fail'),
+(9, 12, '2024-03-11', 12, '12', 'success'),
+(10, 12, '2024-03-11', 12, '12', 'success'),
+(11, 13, '2024-03-11', 12, '12', 'pending'),
+(12, 12, '2024-03-11', 12, '12', 'success'),
+(13, 9999, '2024-03-11', 12, '12', 'pending'),
+(14, 12, '2024-03-11', 12, '12', 'success'),
+(15, 12, '2024-03-11', 12, '12', 'success'),
+(16, 12, '2024-03-11', 12, '12', 'success'),
+(17, 12, '2024-03-11', 12, '12', 'success'),
+(18, 12, '2024-03-11', 12, '12', 'success'),
+(19, 12, '2024-03-11', 12, '12', 'success'),
+(21, 12, '2024-03-11', 12, '12', 'success'),
+(22, 12, '2024-03-11', 12, '12', 'success'),
+(23, 12, '2024-03-11', 12, '12', 'success'),
+(24, 12, '2024-03-11', 12, '12', 'success'),
+(25, 12, '2024-03-11', 12, '12', 'success'),
+(27, 23, '2024-03-05', 23, '23', 'success');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `paymentmethod`
+--
+ALTER TABLE `paymentmethod`
+  ADD PRIMARY KEY (`PaymentMID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `paymentmethod`
+--
+ALTER TABLE `paymentmethod`
+  MODIFY `PaymentMID` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
